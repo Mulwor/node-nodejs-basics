1cce20c6956d4db8f5ee86b33959a69c2367569c
Mulwor <adigezalli@yandex.ru>
2025-12-12 00:58:57 +0300

    update

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+const pathToReadFile = new URL('./files/fileToCompress.txt', import.meta.url)
+const pathToWriteFile = new URL('./files/archive.gz', import.meta.url)
+
+const read = createReadStream(pathToReadFile)
+const gzip = createGzip();
+const write = createWriteStream(pathToWriteFile)
+
 const compress = async () => {
-  // Write your code here
+  await pipeline(read, gzip, write);
+  console.log('GZIP был успешно изменен')
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+const pathToReadFile = new URL('./files/archive.gz', import.meta.url)
+const pathToWriteFile = new URL('./files/fileToCompress.txt', import.meta.url)
+
+const read = createReadStream(pathToReadFile)
+const gzip = createGunzip();
+const write = createWriteStream(pathToWriteFile)
+
 const decompress = async () => {
-  // Write your code here
+  await pipeline(read, gzip, write);
 };
 
 await decompress();
